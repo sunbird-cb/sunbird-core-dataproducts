06be4a0c328d77d05e0aa9744f5fcb4a24bdee34
Shobhit Vashistha <shobhit.vashistha@tarento.com>
2023-05-17 16:51:37 +0530

    fix

--- batch-models/src/main/scala/org/ekstep/analytics/job/JobFactory.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/job/JobFactory.scala
@@ -1,6 +1,6 @@
 package org.ekstep.analytics.job
 
-import org.ekstep.analytics.dashboard.{CompetencyMetricsJob, UserOrgRedisUpdateJob}
+import org.ekstep.analytics.dashboard.CompetencyMetricsJob
 
 import scala.reflect.runtime.universe
 import org.ekstep.analytics.framework.IJob
@@ -28,8 +28,6 @@ object JobFactory {
         WorkFlowSummarizer
       case "competency-metrics" =>
         CompetencyMetricsJob
-      case "user-org-redis-update" =>
-        UserOrgRedisUpdateJob
       case "video-streaming" =>
         VideoStreamingJob
       case "telemetry-replay" =>
