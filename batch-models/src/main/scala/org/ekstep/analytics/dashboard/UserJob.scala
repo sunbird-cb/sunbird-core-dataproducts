a6e8b6ec36ac72e723927ace7794fe26255ef5c8
Shobhit Vashistha <shobhit.vashistha@tarento.com>
2023-07-18 14:14:14 +0530

    fix

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/UserJob.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/UserJob.scala
@@ -1,16 +1,17 @@
 package org.ekstep.analytics.dashboard
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.DataFrame
-import org.ekstep.analytics.dashboard.UserModel.getReportingFrameworkContext
-import org.ekstep.analytics.exhaust.UserAssessmentReportJob.getStorageConfig
 import org.ekstep.analytics.framework.util.DatasetUtil.extensions
 import org.ekstep.analytics.framework.util.{JSONUtils, JobLogger}
-import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobConfig}
-import org.sunbird.cloud.storage.conf.AppConf
+import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobConfig, StorageConfig}
 import org.ekstep.analytics.framework.Level.INFO
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 import org.joda.time.DateTimeZone
 import org.apache.spark.sql.functions.current_timestamp
+import org.ekstep.analytics.framework.conf.AppConf
+import org.ekstep.analytics.framework.storage.CustomS3StorageService
+import org.sunbird.cloud.storage.IStorageService
+
 
 object UserJob extends IJob {
 
@@ -41,20 +42,31 @@ object UserJob extends IJob {
     val storageConfig = getStorageConfig(jobConfig,objectKey)
     JobLogger.log(s"Uploading reports to blob storage", None, INFO)
     println("Uploading reports to blob storage")
+
+    val storageService = StorageUtil.getStorageService(jobConfig)
+    // storageService.put("igotlogs", )
+
     reportData.repartition(1).saveToBlobStore(storageConfig, "csv", s"report-${getDate}",
-      Option(Map("header" -> "true")), Option(Seq("user_org_id")),  None, Option(false) , None, None)
+      Option(Map("header" -> "true")), Option(Seq("user_org_id")),  Option(storageService), Option(false) , None, Option("csv"))
     println("Completed upload to blob ")
     reportData
   }
 
+
+
+
   def getStorageConfig(config: JobConfig, key: String): StorageConfig = {
 
+    // val storageType = AppConf.getConfig("cloud_storage_type")
+
+    val storageEndpoint = AppConf.getConfig("cloud_storage_endpoint_with_protocol")
     val modelParams = config.modelParams.getOrElse(Map[String, Option[AnyRef]]());
-    val container = "igotbm"
+    val container = "igotlogs"
     val storageKey = modelParams.getOrElse("storageKeyConfig", "reports_storage_key").asInstanceOf[String];
     val storageSecret = modelParams.getOrElse("storageSecretConfig", "reports_storage_secret").asInstanceOf[String];
-    val store = modelParams.getOrElse("store", "local").asInstanceOf[String]
-    StorageConfig(store, container, key, Option(storageKey), Option(storageSecret));
+    // val store = modelParams.getOrElse("store", "local").asInstanceOf[String]
+    val store = "s3"
+    StorageConfig(store, container, key, Option(storageKey), Option(storageSecret))
   }
 
   def getDate: String = {
