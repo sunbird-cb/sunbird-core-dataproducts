b279638f187b86b15b7908b638ed674e9d81445f
FsherinP <fathimasherin362@gmail.com>
2024-01-05 16:49:22 +0530

    Added spark job for Default Karma Points

---
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/karma/points/KarmaPointsModel.scala
@@ -0,0 +1,127 @@
+package org.ekstep.analytics.dashboard.karma.points
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
+import org.ekstep.analytics.dashboard.{DashboardConfig, DummyInput, DummyOutput}
+import org.ekstep.analytics.framework.{FrameworkContext, IBatchModelTemplate}
+import org.ekstep.analytics.dashboard.DashboardUtil._
+import org.ekstep.analytics.dashboard.DataUtil._
+
+import java.text.SimpleDateFormat
+import java.util.{Date, UUID}
+
+object KarmaPointsModel extends IBatchModelTemplate[String, DummyInput, DummyOutput, DummyOutput] with Serializable {
+  implicit val className: String = "org.ekstep.analytics.dashboard.KarmaPointsModel"
+  implicit var debug: Boolean = false
+  /**
+   * Pre processing steps before running the algorithm. Few pre-process steps are
+   * 1. Transforming input - Filter/Map etc.
+   * 2. Join/fetch data from LP
+   * 3. Join/Fetch data from Cassandra
+   */
+  override def preProcess(events: RDD[String], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[DummyInput] = {
+    val executionTime = System.currentTimeMillis()
+    sc.parallelize(Seq(DummyInput(executionTime)))
+  }
+
+  /**
+   * Method which runs the actual algorithm
+   */
+  override def algorithm(events: RDD[DummyInput], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[DummyOutput] = {
+    val timestamp = events.first().timestamp // extract timestamp from input
+    implicit val spark: SparkSession = SparkSession.builder.config(sc.getConf).getOrCreate()
+    processKarmaPoints(timestamp, config)
+    sc.parallelize(Seq()) // return empty rdd
+  }
+
+  /**
+   * Post processing on the algorithm output. Some of the post processing steps are
+   * 1. Saving data to Cassandra
+   * 2. Converting to "MeasuredEvent" to be able to dispatch to Kafka or any output dispatcher
+   * 3. Transform into a structure that can be input to another data product
+   */
+  override def postProcess(events: RDD[DummyOutput], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[DummyOutput] = {
+    sc.parallelize(Seq())
+  }
+
+  def processKarmaPoints(timestamp: Long, config: Map[String, AnyRef]) (implicit spark: SparkSession, sc: SparkContext, fc: FrameworkContext): Unit =
+  {
+    // parse model config
+    println(config)
+    implicit val conf: DashboardConfig = parseConfig(config)
+    if (conf.debug == "true") debug = true // set debug to true if explicitly specified in the config
+    if(conf.validation == "true") validation = true
+
+    val dateFormatWithTime = "yyyy-MM-dd 23:59:59"
+    val monthStart = date_format(date_trunc("MONTH", add_months(current_date(), -1)), dateFormatWithTime)
+    val monthEnd = date_format(last_day(add_months(current_date(), -1)), dateFormatWithTime)
+
+    val convertTimeUUIDToDateStringUDF = udf((timeUUID: String) => {
+      val timestamp = (UUID.fromString(timeUUID).timestamp() - 0x01b21dd213814000L) / 10000
+      val date = new Date(timestamp)
+      val dateFormat = new SimpleDateFormat(dateFormatWithTime)
+      dateFormat.format(date)
+    })
+
+    // get cbp details like course name and category
+    val categories = Seq("Course", "Program", "Blended Program", "CuratedCollections", "Standalone Assessment","Curated Program")
+    val cbpDetails = allCourseProgramESDataFrame(categories).select(col("courseID"), col("courseName"), col("category"))
+
+    // Assign 2 karma points for each rating
+    val courseRatingKarmaPointsDF = userCourseRatingDataframe()
+      .withColumn("credit_date", convertTimeUUIDToDateStringUDF(col("createdOn"))).filter(col("credit_date").between(monthStart, monthEnd))
+    val karmaPointsFromRating = courseRatingKarmaPointsDF.join(cbpDetails, Seq("courseID"), "left")
+    val ratingDF = karmaPointsFromRating
+      .withColumn("operation_type", lit("RATING"))
+      .withColumn("addinfo", to_json(map(lit("COURSENAME"), col("courseName"))))
+      .withColumn("points", lit(2).cast("int"))
+      .select(col("courseID").alias("context_id"), col("userID").alias("userid"),
+        col("category").alias("context_type"), col("credit_date"), col("operation_type"), col("addinfo"), col("points")
+      )
+
+    updateKarmaPoints(ratingDF, conf.cassandraUserKeyspace, conf.cassandraKarmaPointsTable)
+
+    /**
+     * time - 1st dec to 31st dec
+     * pick first 4 course completions in december  - assign 5 points for each completion (max - 20)
+     * check the existence of assessments under those 4 courses - additional 5 if assessments are present
+     */
+
+
+
+//    val windowSpec = Window.partitionBy(col("userID")).orderBy(col("courseCompletedTimestamp"))
+    //    var df = userCourseCompletionDF.withColumn("rw", row_number().over(windowSpec)).filter(col("rw") <= 4).drop("rw")
+
+    val userCBPCompletionDF = userCourseProgramCompletionDataFrame().where(col("dbCompletionStatus").equalTo(2))
+      .filter(col("courseCompletedTimestamp").between(unix_timestamp(monthStart, dateFormatWithTime), unix_timestamp(monthEnd, dateFormatWithTime)))
+    val userCourseCompletionDF = userCBPCompletionDF.join(cbpDetails, Seq("courseID"), "left").where(col("category").equalTo("Course"))
+    val firstCompletionDataDF = userCourseCompletionDF.groupByLimit("userID", "courseCompletedTimestamp", 4).drop("rowNum")
+    val userAssessmentDF = userAssessmentDataFrame().select(col("assessChildID"), col("courseID"))
+    var karmaPointsFromCourseCompletion = firstCompletionDataDF.join(userAssessmentDF, Seq("courseID"), "left")
+      .withColumn("operation_type", lit("COURSE_COMPLETION"))
+      .withColumn("hasAssessment", when(col("assessChildID").isNotNull, true).otherwise(false))
+      .withColumn("COURSE_COMPLETION", lit(true))
+      .withColumn("COURSENAME", col("courseName"))
+      .withColumn("ACBP", lit(false))
+      .withColumn("ASSESSMENT", col("hasAssessment"))
+      .withMapColumn("additionalInfo", Seq("COURSE_COMPLETION", "COURSENAME", "ACBP", "ASSESSMENT"))
+
+    show(karmaPointsFromCourseCompletion, "after map column")
+      karmaPointsFromCourseCompletion = karmaPointsFromCourseCompletion
+      .withColumn("addinfo", to_json(col("additionalInfo")))
+//      .withColumn("addInfo", to_json(map(lit("COURSE_COMPLETION"), lit(true), lit("COURSENAME"), col("courseName"), lit("ACBP"), lit("false"), lit("ASSESSMENT"), col("hasAssessment"))))
+      .withColumn("points", when(col("hasAssessment"), lit(10)).otherwise(lit(5)))
+
+      karmaPointsFromCourseCompletion = karmaPointsFromCourseCompletion.select(
+        col("userID").alias("userid"),
+        col("courseID").alias("context_id"),
+        col("category").alias("context_type"),
+        col("courseCompletedTimestamp").alias("credit_date"),
+        col("addinfo"),col("points"),col("operation_type")
+      )
+    updateKarmaPoints(karmaPointsFromCourseCompletion, conf.cassandraUserKeyspace, conf.cassandraKarmaPointsTable)
+//    df.coalesce(1).write.mode(SaveMode.Overwrite).format("csv").option("header", true).save("/tmp/kpoints")
+  }
+}
